0c9afea2aa5b91ce24d4ad764799a3be4eebf2b3
jino <jjino@protonmail.com>
2019-11-18 18:27:19 +0530

    terraform first commit

---
+++ terraform/nginx.tf
@@ -0,0 +1,9 @@
+provider "docker" {}
+
+data "docker_registry_image" "image_name" {
+    name = "nginx:latest"
+}
+
+resource "docker_image" "images" {
+    name = "${data.docker_registry_image.image_name.name}"
+}
